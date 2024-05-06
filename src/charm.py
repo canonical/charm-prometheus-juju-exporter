6ccae03df5b011124d6c27213c693816ef7f358d
Andrea Ieri <andrea.ieri@canonical.com>
2024-05-06 21:35:36 +0200

    Rely on the 3/{risk} snap channel for any 3.x controller
    We have decided to close all 3.y/{risk} snap channels since a single
    version should suffice for all 3.x controllers. We therefore need to
    also update the snap channel selection mechanism here.`
    
    Fixes: #54

--- src/charm.py
+++ src/charm.py
@@ -174,8 +174,8 @@ class PrometheusJujuExporterCharm(CharmBase):
             if controller_version.minor == 9:
                 return "2.9/stable"
         elif controller_version.major == 3:
-            if controller_version.minor in [1, 2, 3, 4, 5]:
-                return f"3.{controller_version.minor}/stable"
+            if controller_version.minor in range(1, 6):
+                return f"3/stable"
 
         raise ControllerIncompatibleError(
             f"Juju controller version {str(controller_version)} is not supported. "
